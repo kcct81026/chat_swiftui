f23b5556f49f2f17c4c53f0f756a3229e8608eb0
KhinCho <khinchotint@onenex.co>
2024-12-04 21:31:53 +0630

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Kcct on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
